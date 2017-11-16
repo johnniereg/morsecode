17b2c37e058c929ae9b784315af51a581b5662db
Johnnie Regalado <johnniereg@gmail.com>
2017-11-16 02:52:01 +0000

    Created variable to give unique names to files.
    Now each entry will be saved as a .txt file with a name that is equivalent to the input string, so that each conversion can be identified.

--- morse.js
+++ morse.js
@@ -42,29 +42,45 @@ const morseCode = {
 
 
 function convertToMorse(string) {
-
+  // Takes the input, removes non-alphanumeric or spaces.
+  // Converts to lower case and puts each character into an array.
   let brokenUpLetters = string.replace(/[^0-9a-z\s]/gi, '').toLowerCase().split("");
 
+  let fileName = string.replace(/[^0-9a-z]/gi, '').toLowerCase();
+  console.log(fileName);
+
+  // Variable to hold converted characters.
   let morseArray = [];
 
+  // Loop over the array of input characters and put their converted
+  // versions into the morseArray.
   brokenUpLetters.forEach(function(character) {
     morseArray.push(morseCode[character]);
   });
 
+  // Turn the morse array into a string, with spaces between words
+  // as per standard morse code convention.
   let morseCodeTranslation = morseArray.join(" ");
 
-  fs.writeFile("results/morsecode.txt", morseCodeTranslation, function (err) {
+  // Write a file usig the input as the file name and save
+  // in the results folder that contains the translated string.
+  fs.writeFile(`results/${fileName}.txt`, morseCodeTranslation, function (err) {
     if (err) throw err;
     console.log("Your code has been saved.");
   } );
 }
 
+// Holds user input.
 let userInput = process.argv;
 
+// Makes sure user input isn't empty.
 if (userInput.length <= 2) {
   console.log(`Please provide an input to convert: <node morse.js "Convert this">`);
+
+// Makes sure there is only one argument being passed in.
 } else if (userInput.length >= 4) {
   console.log(`Put your whole input in a set of quotes: <node morse.js "Convert this">`);
+
 } else {
   convertToMorse(userInput[2]);
 }
